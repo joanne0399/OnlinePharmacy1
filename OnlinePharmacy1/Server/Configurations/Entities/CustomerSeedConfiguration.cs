cd2452d9b0f5b583e1d2f7fcf020dd5b1a69a066
JOANNE SIN SHU JING <2103346I@student.tp.edu.sg>
2023-01-16 23:55:21 +0800

    configure seeding data ok

---
+++ OnlinePharmacy1/Server/Configurations/Entities/CustomerSeedConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlinePharmacy1.Shared.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlinePharmacy1.Server.Configurations.Entities
+{
+    public class CustomerSeedConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.HasData(
+                new Customer
+                {
+                    CustomerID = 1,
+                    CustomerName = "Jennie",
+                    CustomerNRIC = "S9523810J",
+                    CustomerEmail = "Jennieninini@gmail.com",
+                    CustomerAddress = "Bedok North Rd Blk 428 #09-387",
+                    CustomerContactNo = "90818473",
+                    CustomerDOB = "15 January 1995"
+                },
+                new Customer
+                {
+                    CustomerID = 2,
+                    CustomerName = "Jay",
+                    CustomerNRIC = "S9901807D",
+                    CustomerEmail = "Jayjj@gmail.com",
+                    CustomerAddress = "Sin Ming Ave Blk 76 #14-902",
+                    CustomerContactNo = "98017489",
+                    CustomerDOB = "23 November 1999"
+                }
+            );
+        }
+    }
+}
