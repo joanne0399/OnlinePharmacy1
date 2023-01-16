840f66750f5fa391c3360dc95a2e66f37f2ecc49
JOANNE SIN SHU JING <2103346I@student.tp.edu.sg>
2023-01-16 17:30:46 +0800

    identity server

---
+++ OnlinePharmacy1/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OnlinePharmacy1.Server.Data;
+using OnlinePharmacy1.Server.Models;
+
+[assembly: HostingStartup(typeof(OnlinePharmacy1.Server.Areas.Identity.IdentityHostingStartup))]
+namespace OnlinePharmacy1.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
