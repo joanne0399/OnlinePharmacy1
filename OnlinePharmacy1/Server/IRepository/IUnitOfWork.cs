b26d20f4708aa383bbaaf4594b8870006b5d16ed
LEW ZI XIAN <2102192H@student.tp.edu.sg>
2023-01-17 16:38:05 +0800

    IRepository/Repository

---
+++ OnlinePharmacy1/Server/IRepository/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using OnlinePharmacy1.Shared.Domain;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlinePharmacy1.Server.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task Save(HttpContext httpContext);
+        IGenericRepository<Consultation> Consultations { get; }
+        IGenericRepository<Medication> Medications { get; }
+        IGenericRepository<Order> Orders { get; }
+        IGenericRepository<OrderItem> OrderItems { get; }
+        IGenericRepository<Staff> Staffs { get; }
+        IGenericRepository<Customer> Customers { get; }
+    }
+}
