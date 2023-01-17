6fd2b6da3f9c8c1dee00738dc7d20bee1be1a88a
LEW ZI XIAN <117067773+qwerdfqwerdf123@users.noreply.github.com>
2023-01-17 18:11:57 +0800

    Consultation index (#9)

---
+++ OnlinePharmacy1/Client/Static/Endpoints.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlinePharmacy1.Client.Static
+{
+    public static class Endpoints
+    {
+        private static readonly string Prefix = "api";
+
+        public static readonly string ConsultationsEndpoint = $"{Prefix}/consultations";
+        public static readonly string CustomersEndpoint = $"{Prefix}/customers";
+        public static readonly string MedicationsEndpoint = $"{Prefix}/medications";
+        public static readonly string OrdersEndpoint = $"{Prefix}/orders";
+        public static readonly string OrderItemEndpoint = $"{Prefix}/orderitems";
+        public static readonly string StaffsEndpoint = $"{Prefix}/staffs";
+    }
+}
